a7ecd652d5b7c19b9d719e71850bb2041c89e937
jlaguilargomez <jlaguigom@gmail.com>
2020-12-19 11:50:37 +0100

    Added base navigation to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.scss';
 
+const HatsPage = () => (
+  <h1>HATS PAGE</h1>
+)
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
