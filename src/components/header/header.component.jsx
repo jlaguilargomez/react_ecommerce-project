302cb2972d2a6d5ad3fc0e6079f6d413369f9d95
jlaguilargomez <jlaguigom@gmail.com>
2021-03-22 08:18:01 +0100

    Create CartIcon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import CartIcon from '../cart-icon/cart.icon.component';
 
 const Header = ({ currentUser }) => {
   return (
@@ -31,6 +32,7 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
