3d1ecbce407e8664dfb00ee5daac1e35bf61c051
jlaguilargomez <jlaguigom@gmail.com>
2021-03-22 08:26:47 +0100

    Create CartDropdown component (redux not implemented yet)

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,6 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import './cart.icon.styles.scss';
+import './cart-icon.styles.scss';
 
 const CartIcon = () => (
   <div className='cart-icon'>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,8 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import CartIcon from '../cart-icon/cart.icon.component';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ({ currentUser }) => {
   return (
@@ -34,6 +35,7 @@ const Header = ({ currentUser }) => {
         )}
         <CartIcon />
       </div>
+      <CartDropdown />
     </div>
   );
 };
