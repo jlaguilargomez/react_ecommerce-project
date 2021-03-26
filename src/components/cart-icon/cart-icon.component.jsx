1e8b6440a46f78875f64fa16d45e396c7436eb12
jlaguilargomez <jlaguigom@gmail.com>
2021-03-26 16:51:08 +0100

    Display total amount of elements in the cart at cart-icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,16 +5,23 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartDropdown }) => (
-  <div className='cart-icon' onClick={toggleCartDropdown}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
-  </div>
-);
+const CartIcon = ({ toggleCartDropdown, itemCount }) => {
+  console.log('cartItems: ', itemCount);
+  return (
+    <div className='cart-icon' onClick={toggleCartDropdown}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, cartItem) => (acc += cartItem.quantity), 0)
+});
+
 // el primer parámetro, null, es el estado por defecto
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
